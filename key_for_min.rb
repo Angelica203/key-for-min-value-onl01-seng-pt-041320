1eb302d18becb874fbe0cbd17754206cec7cf565
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-26 17:39:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,6 @@
 name_hash = :blake => 500, :ashley => 2, :adam => 1
 
 def key_for_min_value(name_hash)
-  smallest_value = []
-  smallest_key = []
   name_hash.each do |name, num|
     if 
   # name_hash = []
