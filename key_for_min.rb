8fc9880bb011a5e400dca1425f31fa4cc8f4b1c0
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-26 17:31:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+:blake => 500, :ashley => 2, :adam => 1
+
 def key_for_min_value(name_hash)
   # name_hash = []
 
